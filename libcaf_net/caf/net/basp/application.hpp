e6d5a4e15cceae2b067c31ac7ebdcb36080e304d
Jakob Otto <jakob.otto@haw-hamburg.de>
2019-11-22 10:04:50 +0100

    Fix documentation

--- libcaf_net/caf/net/basp/application.hpp
+++ libcaf_net/caf/net/basp/application.hpp
@@ -85,12 +85,9 @@ public:
     // Allow unit tests to run the application without endpoint manager.
     if constexpr (!std::is_base_of<test_tag, Parent>::value)
       manager_ = &parent.manager();
-    size_t workers;
-    if (auto workers_cfg = get_if<size_t>(&system_->config(),
-                                          "middleman.workers"))
-      workers = *workers_cfg;
-    else
-      workers = std::min(3u, std::thread::hardware_concurrency() / 4u) + 1;
+    auto workers = get_or(system_->config(), "middleman.workers",
+                          defaults::middleman::workers);
+    CAF_LOG_DEBUG("using " << CAF_ARG(workers) << " for deserializing");
     for (size_t i = 0; i < workers; ++i)
       hub_->add_new_worker(*queue_, proxies_);
     // Write handshake.
